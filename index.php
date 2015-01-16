31e5a5a2e0e52bdecc18547442471a6b1b0ac15b
Chris Fuhrman <chrisfuhrman@gmail.com>
2015-01-16 14:45:19 -0600

    fixed script tag

--- index.php
+++ index.php
@@ -26,149 +26,125 @@
         <span class="display-hide" id="timer"></span>
     </div>
 
-<div class="container">
-    
-    <h1>Whack a cat</h1>
+    <div class="container">
+        
+        <h1>Whack a cat</h1>
 
-    <div id="button-container">
-        <button type="button" class="btn btn-primary btn-lg" id="btn-start">Start Game</button>
-        <button type="button" class="btn btn-primary btn-lg display-hide" id="btn-stop">Stop Game</button>
+        <div id="button-container">
+            <button type="button" class="btn btn-primary btn-lg" id="btn-start">Start Game</button>
+            <button type="button" class="btn btn-primary btn-lg display-hide" id="btn-stop">Stop Game</button>
+        </div>
+
+        <div id="final-content">
+            <p id="final-message"></p>
+            <p id="final-score"></p>
+        </div>
+
+       <div id="button-container">
+            <button type="button" class="display-hide btn btn-primary btn-lg" id="btn-again">Play again?</button>
+        </div>
+        
+        <audio id="cat-audio" src="aud/cat.mp3"></audio>
+        
+        <div id="game-container">
+
+            <div class="grid custom-cursor" id="box-0">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class="grid" id="box-1">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-2">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-3">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-4">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-5">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-6">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-7">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-8">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-9">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-10">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-11">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-12">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-13">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-14">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-15">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-16">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-17">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-18">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-19">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-20">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            <div class ="grid" id="box-21">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+           <div class ="grid" id="box-22">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+           <div class ="grid" id="box-23">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+           <div class ="grid" id="box-24">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+           <div class ="grid" id="box-25">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+           <div class ="grid" id="box-26">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+           <div class ="grid" id="box-27">
+                <img src="img/cat_small.png" class="display-hide cat">
+            </div>
+            
+
+            <div class="clear"></div>
+
+        </div>
     </div>
-
-    <div id="final-content">
-        <p id="final-message"></p>
-        <p id="final-score"></p>
-    </div>
-
-   <div id="button-container">
-        <button type="button" class="display-hide btn btn-primary btn-lg" id="btn-again">Play again?</button>
-    </div>
-    
-    <audio id="cat-audio" src="aud/cat.mp3"></audio>
-    
-    <div id="game-container">
-
-        <div class="grid custom-cursor" id="box-0">
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class="grid" id="box-1">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-2">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-3">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-4">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-5">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-6">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-7">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-8">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-9">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-10">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-11">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-12">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-13">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-14">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-15">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-16">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-17">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-18">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-19">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-20">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-        <div class ="grid" id="box-21">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-       <div class ="grid" id="box-22">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-       <div class ="grid" id="box-23">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-       <div class ="grid" id="box-24">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-       <div class ="grid" id="box-25">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-       <div class ="grid" id="box-26">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-       <div class ="grid" id="box-27">
-            
-            <img src="img/cat_small.png" class="display-hide cat">
-        </div>
-
-        <div class="clear"></div>
-
-    </div>
-</div>
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-<?php require_once 'js/script.js'; ?>
+<script src="js/script.js"></script>
+
+
 
 </body>
 </html>
